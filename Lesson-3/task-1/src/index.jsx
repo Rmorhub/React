cc23ab4a11a8a92400208f59fff81e31fe5fe9ac
Rmorhub <rmorphone@gmail.com>
2021-09-21 16:07:14 +0300

    task-1

--- Lesson-3/task-1/src/index.jsx
+++ Lesson-3/task-1/src/index.jsx
@@ -5,7 +5,7 @@ import './search.scss';
 
 const rootElement = document.querySelector('#root');
 
-const SearchField = (props) => {
+export default (props) => {
   return (
     <div className="search">
       <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
