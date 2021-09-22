721bbe1ddf78dc0fc3e3251ea8efc95a7d32c95a
Rmorhub <rmorphone@gmail.com>
2021-09-22 12:51:51 +0300

    task-5

---
+++ Lesson-3/task-5/src/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import moment from 'moment';
+
+const Profile = (props) => {
+  const formatDate = (date) => moment(date).format('DD MMM YY');
+  const date = formatDate(props.userData.birthDate);
+
+  return (
+    <>
+      <div className="profile__name">{`${props.userData.firstName} ${props.userData.lastName}`}</div>
+      <div className="profile__birth">{`Was born ${date} in ${props.userData.birthPlace}`}</div>
+    </>
+  );
+};
+
+export default Profile;
---
+++ Lesson-3/task-5/src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './Profile';
+import './index.scss'
+
+const userData = {
+  firstName: 'John',
+  lastName: 'Doe',
+  birthDate: '1991-01-17T11:11:11.819Z',
+  birthPlace: 'London',
+};
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Profile userData={userData} />, rootElement);
+
+
+
