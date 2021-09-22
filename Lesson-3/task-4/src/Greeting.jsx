2676801f46b2adfcb55c17176b6b391cc53945fd
Rmorhub <rmorphone@gmail.com>
2021-09-22 13:37:41 +0300

    task-4

--- Lesson-3/task-4/src/Greeting.jsx
+++ Lesson-3/task-4/src/Greeting.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import moment from 'moment';
 
 const Greeting = (props) => {
-  const diff = new Date() - props.birthDate;
-  const age = new Date(diff).getFullYear() - 1970;
+  // const diff = new Date() - props.birthDate;
+  // const age = new Date(diff).getFullYear() - 1970;
+  const age = moment().diff(moment(props.birthDate), 'Years');
 
   return (
     <div className="greeting">{`My name is ${props.firstName} ${props.lastName}. I'm ${age} years old`}</div>
