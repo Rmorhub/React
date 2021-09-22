2196e05c8f51c991fc627b5ff2702722bb177148
Rmorhub <rmorphone@gmail.com>
2021-09-22 17:27:06 +0300

    task-2

--- Lesson-5/task-2/src/GoodButton.jsx
+++ Lesson-5/task-2/src/GoodButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class GoodButton extends React.Component {
   handleClick(event) {
-    alert(event.target);
+    alert(event.target.textContent);
   }
   render() {
     return (
