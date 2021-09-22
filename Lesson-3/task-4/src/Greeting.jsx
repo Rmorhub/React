afe599ac7c7d4c9f1a1b569f68cb08f9206761b2
Rmorhub <rmorphone@gmail.com>
2021-09-22 11:02:14 +0300

    task-4

--- Lesson-3/task-4/src/Greeting.jsx
+++ Lesson-3/task-4/src/Greeting.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+const age = (date) => {
+  const diff = new Date() - date;
+  const age = new Date(diff).getFullYear() - 1970;
+  return age;
+};
+
 const Greeting = (props) => {
   return (
-    <div className="greeting">{`My name is ${props.user.firtstName} ${props.user.lastName}. I'm ${props.user.birthDate} years old`}</div>
+    <div className="greeting">{`My name is ${props.firtstName} ${props.lastName}. I'm ${age(props.birthDate)} years old`}</div>
   );
 };
 
--- Lesson-3/task-4/src/index.jsx
+++ Lesson-3/task-4/src/index.jsx
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom';
 import Greeting from './Greeting';
 import './index.scss';
 
-const birthDay = new Date('2001-01-01T11:11:11.819Z');
-
-const age = (date) => {
-  const diff = new Date() - date;
-  const age = new Date(diff).getFullYear() - 1970;
-  return age;
-};
-
-const johnDoe = {
-  firtstName: 'John',
-  lastName: 'Doe',
-  birthDate: age(birthDay),
-};
-
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Greeting user={johnDoe} />, rootElement);
+ReactDOM.render(
+  <Greeting
+    firtstName="John"
+    lastName="Doe"
+    birthDate={new Date('2001-01-01T11:11:11.819Z')}
+  />,
+  rootElement
+);
