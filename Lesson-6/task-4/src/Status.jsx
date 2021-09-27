401ff66b70425a138eb1123141873666d8f1ab7b
Rmorhub <rmorphone@gmail.com>
2021-09-27 16:31:06 +0300

    task-4

---
+++ Lesson-6/task-4/src/Status.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Offline from './Offline';
+import Online from './Online';
+
+class Status extends Component {
+  state = {
+    isOnline: false,
+  };
+
+  render() {
+    return (
+      <div className="status">
+        {this.state.isOnline ? <Online /> : <Offline />}
+      </div>
+    );
+  }
+}
+
+export default Status;
