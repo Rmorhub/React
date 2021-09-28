70d80d1674e0dfcd9e733f17e5150c6a7f4da90b
Rmorhub <rmorphone@gmail.com>
2021-09-28 20:24:14 +0300

    task-4

--- Lesson-7/task-4/src/Pagination.jsx
+++ Lesson-7/task-4/src/Pagination.jsx
@@ -7,26 +7,29 @@ const Pagination = ({
   totalItems,
   itemPerPage,
 }) => {
-  const isPrevPageAvailable = totalItems % itemPerPage !== 0;
-  const isNextPageAvailable = totalItems % itemPerPage === 0;
-  const maxPage = Math.ceil(totalItems / itemPerPage);
+  const isPrevPageAvailable =
+    currentPage !== 1 ? (
+      <button className="btn" onClick={goPrev}>
+        ←
+      </button>
+    ) : (
+      <button className="btn" onClick={goPrev} disabled={true}></button>
+    );
+
+  const isNextPageAvailable =
+    totalItems / itemPerPage > currentPage ? (
+      <button className="btn" onClick={goNext}>
+        →
+      </button>
+    ) : (
+      <button className="btn" onClick={goNext} disabled={true}></button>
+    );
+
   return (
     <div className="pagination">
-      <button
-        className="btn"
-        onClick={goPrev}
-        disabled={currentPage === 1 ? isPrevPageAvailable : false}
-      >
-        {currentPage === 1 ? '' : '←'}
-      </button>
+      {isPrevPageAvailable}
       <span className="pagination__page">{currentPage}</span>
-      <button
-        className="btn"
-        onClick={goNext}
-        disabled={currentPage === maxPage ? isPrevPageAvailable : false}
-      >
-        {currentPage === maxPage ? '' : '→'}
-      </button>
+      {isNextPageAvailable}
     </div>
   );
 };
