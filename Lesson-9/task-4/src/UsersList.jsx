9de242a7338f8132d2f5ab16a36bdc89ca9de921
Rmorhub <rmorphone@gmail.com>
2021-09-30 19:50:33 +0300

    task-4

---
+++ Lesson-9/task-4/src/UsersList.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Filter from './Filter';
+import User from './User';
+
+class UserList extends Component {
+  state = {
+    value: '',
+  };
+  onChange = (event) => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+  render() {
+    const filterUsers = !this.state.value
+      ? this.props.users
+      : this.props.users.filter(
+          ({ name }) => name.toLowerCase() === this.state.value.toLowerCase()
+        );
+
+    const users = filterUsers.map(({ id, name, age }) => (
+      <User key={id} name={name} age={age} />
+    ));
+
+    return (
+      <div>
+        <Filter
+          filterText={this.state.value}
+          onChange={this.onChange}
+          count={users.length}
+        />
+        <div className="users">{users}</div>
+      </div>
+    );
+  }
+}
+
+export default UserList;
