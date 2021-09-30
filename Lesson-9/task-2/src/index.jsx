52821e9a92b795ccc1f22473a5b364d95bc8036a
Rmorhub <rmorphone@gmail.com>
2021-09-30 09:21:41 +0300

    task-2

---
+++ Lesson-9/task-2/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import UserForm from './UserForm';
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<UserForm />, rootElement);
