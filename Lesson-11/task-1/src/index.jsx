1ec10193096408ddec4880450bdd522da8653d5d
Rmorhub <rmorphone@gmail.com>
2021-10-05 11:46:44 +0300

    task-1

---
+++ Lesson-11/task-1/src/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<App />, rootElement);
+
+{
+  /* <div class="app">
+  <!-- OddNumbers -->
+  <div class="number">
+    <span class="number__title">Odd number</span>
+    <span class="number__value">83</span>
+  </div>
+  <!-- EvenNumbers -->
+  <div class="number">
+    <span class="number__title">Even number</span>
+    <span class="number__value">82</span>
+  </div>
+  <!-- Numbers -->
+  <div class="number">
+    <span class="number__title">All numbers</span>
+    <span class="number__value">83</span>
+  </div>
+  <!-- Numbers -->
+  <div class="number">
+    <span class="number__title">Just 17</span>
+    <span class="number__value">17</span>
+  </div>
+</div> */
+}
