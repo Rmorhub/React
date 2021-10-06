b255873058bd881e045014592177275526fb273f
Rmorhub <rmorphone@gmail.com>
2021-10-06 18:47:23 +0300

    task-1

---
+++ Lesson-12/task-1/src/TodoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskList from './TaskList.jsx';
+
+const TodoList = () => {
+  return (
+    <>
+      <h1 className="title">Todo List </h1>
+      <TaskList />
+    </>
+  );
+};
+
+export default TodoList;
