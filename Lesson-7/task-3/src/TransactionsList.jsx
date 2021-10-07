ddc852c57a29482424bbfe2ed43cc404e73ce92f
Rmorhub <rmorphone@gmail.com>
2021-10-07 12:32:42 +0300

    task-3

--- Lesson-7/task-3/src/TransactionsList.jsx
+++ Lesson-7/task-3/src/TransactionsList.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Transaction from './Transaction';
 
-const TransactionsList = ({ transactions }) => {
-  return (
+const TransactionsList = ({ transactions }) => (
     <ul className="transactions">
       {transactions.map((transaction) => (
         <Transaction key={transaction.id} {...transaction} />
       ))}
     </ul>
   );
-};
 
 export default TransactionsList;
