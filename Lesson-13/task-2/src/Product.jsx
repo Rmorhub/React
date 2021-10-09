59fe3b4a6949e8b4d76e9bede1bbf3a52d73ad96
Rmorhub <rmorphone@gmail.com>
2021-10-09 12:27:13 +0300

    task-2

--- Lesson-13/task-2/src/Product.jsx
+++ Lesson-13/task-2/src/Product.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 
 function Contacts() {
   const { productId } = useParams();
-  return <div className="product">{`Product is a ${productId}`}</div>;
+  return <div className="product">{productId}</div>;
 }
 
 export default Contacts;
